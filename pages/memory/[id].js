f024b5d61f9c72dec9a9c0ade13a53d5bacfbf30
suvel <suvelrathneswar08@gmail.com>
2022-05-31 22:45:29 +0530

    Added Detail page

---
+++ pages/memory/[id].js
@@ -0,0 +1,34 @@
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import { useState, useEffect } from 'react';
+import memories from '../../data/info.js'
+import styles from '../../styles/memory.module.css'
+export default function Memory(props) {
+    const { id } = useRouter().query;
+    const [memory, setMemory] = useState({});
+
+    useEffect(() => {
+        console.log(memories)
+        const selectedMemory = memories.find(mem => mem.id == id)
+        console.log(selectedMemory)
+        setMemory(selectedMemory)
+    }, [id])
+
+    useEffect(() => {
+        console.log(memory)
+    }, [memory])
+
+    return (<div className='memory'>
+        <h1 className='memory_title'>{memory?.title}</h1>
+        <h2 className='memory_loc'>{memory?.location}</h2>
+        <div className={styles.memory_imgContainer}>
+            <Image
+                className='memory_img'
+                layout='fill'
+                src={memory?.image || "https://picsum.photos/200"} />
+        </div>
+        <p className='memory_desc'>
+            {memory?.description}
+        </p>
+    </div>)
+}
