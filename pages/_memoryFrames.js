d73be277dd8f91f2cef042e96ae73a9dbb9b45dd
suvel <suvelrathneswar08@gmail.com>
2022-05-31 23:33:59 +0530

    bufix, build issue

--- pages/_memoryFrames.js
+++ pages/_memoryFrames.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import Polaroid from './_polaroid.js'
 
-const MemoryFrames = ({ memories }) => {
-    return (memories.map(memory => {
-        return (
-            <Polaroid
-                key={memory.id}
-                image={memory.image}
-                title={memory.title}
-                description={memory.description}
-                location={memory.location}
-                geoMark={memory.geoMark}
-            />
-        )
-    }))
+const MemoryFrames = ({ memories = [] }) => {
+    if (memories?.length == 0) {
+        return <></>
+    }
+    else {
+        return (memories.map(memory => {
+            return (
+                <Polaroid
+                    key={memory.id}
+                    image={memory.image}
+                    title={memory.title}
+                    description={memory.description}
+                    location={memory.location}
+                    geoMark={memory.geoMark}
+                />
+            )
+        }))
+    }
 }
 
 export default MemoryFrames
